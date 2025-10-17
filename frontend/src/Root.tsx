eac2c16a360ae975e6b891c26540752db52056de
Risman Hadi Nata <rismanh158@gmail.com>
2025-10-17 10:53:25 +0700

    perubahan title

---
+++ frontend/src/Root.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import App from "./App";
+
+const Root: React.FC = () => {
+  useEffect(() => {
+    const getStoreSettings = async () => {
+      try {
+        const res = await fetch("http://192.168.110.16:5000/api/admin/settings");
+        const data = await res.json();
+
+        if (data.storeName) document.title = data.storeName;
+        if (data.storeLogo) {
+          const favicon = document.querySelector("link[rel='icon']") as HTMLLinkElement;
+          if (favicon) favicon.href = data.storeLogo;
+        }
+      } catch (error) {
+        console.error("Gagal mengambil store name:", error);
+      }
+    };
+
+    getStoreSettings();
+  }, []);
+
+  return <App />;
+};
+
+export default Root;
