49a03cd3ccca7bfebb2772d268295e325a7776d5
Risman Hadi Nata <rismanh158@gmail.com>
2025-12-19 17:37:28 +0700

    example firebase-service-account.json

--- backend/ngrokbackend.ts
+++ backend/ngrokbackend.ts
@@ -1,4 +1,4 @@
-const ngrokUrl = `https://add4ce191859.ngrok-free.app`
+const ngrokUrl = `https://5ac7a134ab33.ngrok-free.app`
 const portbe = 5000 //pastikan melakukan stop pada server dan menjalankannya lagi
 
 export {ngrokUrl, portbe}
